4ce0db0e96bd42ccb011c97e37d8c76f9fbee1f0
michele.arciprete <michele.arciprete@e-dea.it>
2018-03-27 14:06:22 +0200

    Add CustomerController and testing

---
+++ src/main/java/it/senape/wldiag/controller/CustomerController.java
@@ -0,0 +1,36 @@
+package it.senape.wldiag.controller;
+
+import it.senape.wldiag.dto.CustomerDto;
+import it.senape.wldiag.service.internal.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+/**
+ * Created by michele.arciprete on 19-Dec-17.
+ */
+@Controller
+@CrossOrigin("http://localhost:4200")
+@RequestMapping(value = "api/customers")
+public class CustomerController {
+
+    private CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public List<CustomerDto> list() {
+        return customerService.findAll();
+    }
+
+}
