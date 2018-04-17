3f453cf9063f3cabc8b75fc2f00b2e339570d60f
michele.arciprete <michele.arciprete@e-dea.it>
2018-04-17 14:33:10 +0200

    Add projection file

---
+++ src/main/java/it/senape/wldiag/jpa/projection/TopClass.java
@@ -0,0 +1,10 @@
+package it.senape.wldiag.jpa.projection;
+
+/**
+ * Created by michele.arciprete on 14-Apr-18
+ */
+public interface TopClass {
+
+    public String getFqnClassName();
+    public Integer getHits();
+}
