ddfd05be49ff312e0791b462afa0fd924e65670f
michele.arciprete <michele.arciprete@e-dea.it>
2018-04-23 10:13:24 +0200

    angular frontend - initial import

--- src/integration-test/java/it/senape/wldiag/jpa/repository/ITInternalThreadRepositoryTest.java
+++ src/integration-test/java/it/senape/wldiag/jpa/repository/ITInternalThreadRepositoryTest.java
@@ -1,11 +1,14 @@
 package it.senape.wldiag.jpa.repository;
 
 import it.senape.wldiag.jpa.model.jta.InternalThread;
+import it.senape.wldiag.jpa.model.jvm.ThreadDump;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
@@ -32,4 +35,11 @@ public class ITInternalThreadRepositoryTest {
         List<InternalThread> threadList = repo.findAllByDiagnosticImageIdAndNameIn(diagnosticImageId, threadNames);
         assertFalse(threadList.isEmpty());
     }
+
+
+//    @Test
+//    public void findAllDumpedThreads() throws Exception {
+//        Page<ThreadDump> dumpedThreads = repo.findAllDumpedThreads(PageRequest.of(0,10));
+//        assertFalse(dumpedThreads.getContent().isEmpty());
+//    }
 }
